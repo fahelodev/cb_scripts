ead6eb82984c3854704b7d7c02b66acdaffc52d1
Keijan Andres Chaug Encina <kchaug.tsoft@bancochile.cl>
2021-05-02 01:48:29 -0400

    add property por componentName

--- src/dsl/createMasterComponent.groovy
+++ src/dsl/createMasterComponent.groovy
@@ -5,14 +5,19 @@ def processName = "Deploy"
 def processTypeName = "DEPLOY"
 //Variables del subproceso
 def subTaskNames = ["Start-cmd-1", "Run Plan SB-runPlanBamboo-2", 
-"Create Release SB-CreateRelBamboo-2.1", "Run Plan SB-runPlanBamboo-3", 
+"Create Release SB-CreateRelBamboo-2.1", "Run Plan Dev-runPlanBamboo-3", 
 "Create Release Dev-CreateRelBamboo-3.1", "Deploy-proc-4"]
 def paramSubTaskNames = ["command", "planKeySb", "planKeyDev", "projectKeySb","projectKeyDev"]
+def envs = ["Sandbox", "Dev", "QA", "Beta", "Prod", "BF", "DR"]
+def propParamsDefs = ["projectKey", "planKey", "deploymentName", "branchName",
+"branchKey", "artifactId", "namespaceName", "namespaceValue"]
 
 componentNames.each { compName->
     component compName, {
         pluginKey = 'EC-Artifact'
         projectName = projName
+        //Se generan los properties por ambientes
+                
         //Se genera el proceso del componente
         process processName, {
             processType = processTypeName
@@ -32,9 +37,34 @@ componentNames.each { compName->
                     processStep subTaskType[0], {
 
                     }
-                    
+                    subTaskNames.each {
+                        def validation = it.split("-")
+                        if(validation[2] == "1" &&  subTaskType[0].split(" ")[2]== "SB"){
+                            processDependency validation[0], targetProcessStepName: subTaskType[0], {
+                                branchCondition = '"$[/myResource/name]" == "Sandbox"'
+                                branchConditionName = 'if ' + subTaskType[0].split(" ")[2]
+                                branchConditionType = 'CUSTOM'
+                                branchType = 'ALWAYS'
+                            }
+                        }else if(validation[2] == "1" &&  subTaskType[0].split(" ")[2]== "Dev"){
+                            processDependency validation[0], targetProcessStepName: subTaskType[0], {
+                                branchCondition = '"$[/myResource/name]" == "Dev"'
+                                branchConditionName = 'if ' + subTaskType[0].split(" ")[2]
+                                branchConditionType = 'CUSTOM'
+                                branchType = 'ALWAYS'
+                            }
+                        }
+                    }
                 }else if(subTaskType[1] == 'CreateRelBamboo'){
                     processStep subTaskType[0], {
+                    }
+                    subTaskNames.each {
+                        def validation = it.split("-")
+                        if(validation[2] == "2" &&  subTaskType[0].split(" ")[2]== "SB"){
+                            processDependency validation[0], targetProcessStepName: subTaskType[0], {}
+                        }else if(validation[2] == "3" &&  subTaskType[0].split(" ")[2]== "Dev"){
+                            processDependency validation[0], targetProcessStepName: subTaskType[0], {}
+                        }
                     }
                 }else if(subTaskType[1] == 'proc'){
                     processStep subTaskType[0], {
@@ -42,18 +72,42 @@ componentNames.each { compName->
                     subprocedure = 'Deployment'
                     subproject = projName
                     }
-                    /*subTaskNames.each {
+                    subTaskNames.each {
                         def validation = it.split("-")
-                        if(validation[2] == "1" || validation[2] == "2.1" || validation[2] == "3.1" || )
-                        processDependency 'Start', targetProcessStepName: 'Deploy', {
-                            branchCondition = '\'$[/myResource/name]\' == \'qa\''
-                            branchConditionName = 'if'
-                            branchConditionType = 'CUSTOM'
-                            branchType = 'ALWAYS'
+                        if(validation[2] == "1"){
+                            processDependency validation[0], targetProcessStepName: subTaskType[0], {
+                                branchCondition = '"$[/myResource/name]" != "Dev" && "$[/myResource/name]" != "Sandbox" '
+                                branchConditionName = 'if'
+                                branchConditionType = 'CUSTOM'
+                                branchType = 'ALWAYS'
+                            }
+                        }else if(validation[2] == "2.1" || validation[2] == "3.1"){
+                            processDependency validation[0], targetProcessStepName: subTaskType[0], {
+                                branchCondition = '"$[/myResource/name]" == "Sandbox"'
+                                branchConditionName = 'if'
+                                branchConditionType = 'CUSTOM'
+                                branchType = 'ALWAYS'
+                            }
+                        }else if(validation[2] == "3.1"){
+                            processDependency validation[0], targetProcessStepName: subTaskType[0], {
+                                branchCondition = '"$[/myResource/name]" == "Dev"'
+                                branchConditionName = 'if'
+                                branchConditionType = 'CUSTOM'
+                                branchType = 'ALWAYS'
+                            }
                         }
-                    }*/
+                    }
                 }
             }
+            property compName, {
+            envs.each { envName->
+                property envName, {
+                    propParamsDefs.each { propParamsDef->
+                        property propParamsDef, value: '', {}
+                    }
+                }
+            }
+        }
         }
         //proceso que rellena los valores de artifactName del Componente
         property 'ec_content_details', {
